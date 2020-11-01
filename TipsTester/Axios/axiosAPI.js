34e6806e8c1be0905d97870b300e666d1544c0f9
nirajgole <65242290+nirajgole@users.noreply.github.com>
2020-11-02 00:09:20 +0530

    Add files via upload

--- TipsTester/Axios/axiosAPI.js
+++ TipsTester/Axios/axiosAPI.js
@@ -1,7 +1,9 @@
+const axios = require('axios');
+
 //properties
 //fake API
 const API = "https://reqres.in/api/"; //online
-const myAPI = " http://localhost:3000/"; //offline
+const myAPI = "http://localhost:3000/"; //offline
 
 // function getById(id){
 //     return document.getElementById(""+id);
@@ -11,6 +13,8 @@ const myAPI = " http://localhost:3000/"; //offline
 // Elements
 const getBtn = document.getElementById("get_btn");
 const postBtn = document.getElementById("post_btn");
+const delBtn = document.getElementById('delete_btn');
+
 
 // object creation
 class Blogger {
@@ -63,9 +67,60 @@ var newTime = new Date().toLocaleTimeString();
   console.log(first_name);
 };
 
+//3.Delete Data
+
+// const deleteData=()=>{
+//   var getId = prompt("Enter Data").toLowerCase();
+//   console.log(getId);
+//   axios.delete(myAPI+'users?userName='+getId).then((response)=>{
+//     console.log(response);
+//   });
+// }
+
+const deleteData=()=>{
+  axios.delete('http://localhost:3000/users/1/')
+    .then(resp => {
+        console.log(resp.data)
+    }).catch(error => {
+        console.log(error);
+    });  
+}
+
+//http://localhost:3000/users?userName=101
+
 // click Events
 getBtn.addEventListener("click", getData);
 
 postBtn.addEventListener("click", postData);
 
+delBtn.addEventListener('click',deleteData);
+
 //tutorial link: https://www.youtube.com/watch?v=qM4G1Ai2ZpE&ab_channel=TraversyMedia
+
+
+
+// onDeleteClick(id, e) {
+//   let inventory;
+//   axios.delete(api() + '/products/' + id).then((deleted) => {
+//    axios.get(api() + this.state.lastGET).then((response) => {
+//     inventory = response.data.data
+//     this.setState({inventory})
+//    })
+//   })
+//  }
+
+
+// axios.delete(url, { data: { foo: "bar" } });
+
+
+// axios({
+//   method: 'DELETE',
+//   url: 'http://localhost:3000/posts/1',
+//   headers: { 'Content-Type': 'application/json' },
+// });
+
+// const axiosInstance = axios.create({
+//   baseURL:'http://localhost:3000/'
+// });
+
+// axiosInstance.get(...).then(...);
